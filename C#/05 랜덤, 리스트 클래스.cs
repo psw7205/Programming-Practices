0b93269fdbe1bf76945798c824eb65cbce331893
psw7205 <39735020+psw7205@users.noreply.github.com>
2019-03-28 10:52:16 +0900

    Create 05 랜덤, 리스트 클래스.cs

---
+++ C#/05 랜덤, 리스트 클래스.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                myList.Add(new Random().Next(1, 100));
+            }
+
+            foreach (var item in myList)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
