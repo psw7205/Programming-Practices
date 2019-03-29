e3db60e6ef203b112ed8c834b3d358ec05dd7ada
sang-woo park <psw7205@gmail.com>
2019-03-30 01:17:56 +0900

    add Chapter05

---
+++ C#/Chapter05_3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05
+{
+    class BookInfo
+    {
+        public string bookName;
+        public string firstEditionDate;
+        public string author;
+        public string publisher;
+        public string publish;
+        public string chiefEditor;
+        public string planner;
+        public string editior;
+        public string designer;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookInfo php = new BookInfo()
+            {
+                bookName = "PHP 프로그래밍 입문",
+                firstEditionDate = "2013년 5월 20일",
+                author = "황재호",
+                publisher = "김태헌",
+                publish = "한빛아카데미(주)",
+                chiefEditor = "김현용",
+                planner = "김이화",
+                editior = "김이화",
+                designer = "여동일"
+            };
+
+        }
+    }
+}
---
+++ C#/Chapter05_4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = new Random().Next(0, 500);
+
+            while(true)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                int tmp = int.Parse(Console.ReadLine());
+
+                if(tmp > answer)
+                {
+                    Console.WriteLine(tmp + "보다는 작은 숫자입니다.\n");
+                }
+                else if(tmp < answer)
+                {
+                    Console.WriteLine(tmp + "보다는 큰 숫자 입니다.\n");
+                }
+                else
+                {
+                    Console.WriteLine("정답입니다!!\n");
+                    break;
+                }
+            }
+
+        }
+    }
+}
