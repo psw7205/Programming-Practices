1060a787e4b8602256bf163f94b8df4ff354dd05
unknown <phsu2021@sju.ac.kr>
2019-04-17 16:44:35 +0900

    update

---
+++ C#/08 상속 다형성.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Csharp
+{
+    class Parent
+    {
+        public static int counter = 0;
+        public int var = 10;
+        public void CountParnet()
+        {
+            Parent.counter++;
+        }
+    }
+
+    class Child : Parent
+    {
+        public new string var = "abcd";
+        public void CountChild()
+        {
+            Child.counter++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent parent = new Parent();
+            Child child = new Child();
+
+            parent.CountParnet();
+            child.CountChild();
+            child.CountChild();
+            child.CountChild();
+
+
+            Console.WriteLine(Parent.counter);
+            Console.WriteLine(Child.counter);
+            
+            Console.WriteLine(child.var);
+            Console.WriteLine(((Parent)child).var);
+        }
+    }
+}
