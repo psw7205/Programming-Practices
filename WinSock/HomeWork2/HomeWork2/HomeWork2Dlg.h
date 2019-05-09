a2fe95f77debdf1e668f5ad9dec65d8b37e63fc8
unknown <phsu2021@sju.ac.kr>
2019-05-10 01:06:38 +0900

    update homework2

---
+++ WinSock/HomeWork2/HomeWork2/HomeWork2Dlg.h
@@ -0,0 +1,38 @@
+﻿
+// HomeWork2Dlg.h: 헤더 파일
+//
+
+#pragma once
+#include <string>
+
+// CHomeWork2Dlg 대화 상자
+class CHomeWork2Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CHomeWork2Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HOMEWORK2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	bool isClose = false;
+	int port;
+	std::string ipAddr;
+};
---
+++ WinSock/HomeWork2/HomeWork2/InputMulticast.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "HomeWork2Dlg.h"
+
+// InputMulticast 대화 상자
+
+class InputMulticast : public CDialogEx
+{
+	DECLARE_DYNAMIC(InputMulticast)
+
+public:
+	InputMulticast(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~InputMulticast();
+	 
+
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+
+	CHomeWork2Dlg *mainDlg;
+
+	int result;
+	virtual void OnCancel();
+	CString name;
+	DWORD ipAddr;
+	CEdit port;
+	afx_msg void OnEnUpdateEdit1();
+};
