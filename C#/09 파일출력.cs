b3a6ba18ae39db889f7417516c4694fdb97cd3d2
unknown <phsu2021@sju.ac.kr>
2019-05-16 21:52:38 +0900

    update C#

---
+++ C#/09 파일출력.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter(@"test.txt"))
+            {
+                string line;
+                while (true)
+                {
+                    line = Console.ReadLine();
+                    string tmp = line.ToLower();
+                    if (tmp == "exit")
+                        break;
+
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
