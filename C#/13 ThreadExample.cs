34434c057a8f450af87169392a6957370f60e77c
unknown <phsu2021@sju.ac.kr>
2019-05-31 00:21:41 +0900

    Create 13 ThreadExample.cs

---
+++ C#/13 ThreadExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class ThreadExample
+    {
+        static void Main(string[] args)
+        {
+            int length = 1000;
+            Thread thread = new Thread(() =>
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write("A");
+                }
+            });
+
+
+            Thread thread2 = new Thread(delegate() {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write("B");
+                }
+            });
+
+            Thread thread3 = new Thread(TestMethod);
+
+            thread.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        public static void TestMethod()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("C");
+            }
+        }
+    }
+}
