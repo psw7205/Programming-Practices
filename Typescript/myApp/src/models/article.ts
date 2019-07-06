466efb4f22ae9b7b09490aa9e7b19be3a17fbd4f
psw7205 <psw7205@gmail.com>
2019-07-06 17:07:56 +0900

    create myApp

---
+++ Typescript/myApp/src/models/article.ts
@@ -0,0 +1,8 @@
+export class Article {
+  public author: string;
+  public title: string;
+  public desc: string;
+  public url: string;
+  public urlToImg: string;
+  public date: string;
+}
---
+++ Typescript/myApp/src/models/news.ts
@@ -0,0 +1,8 @@
+import { Article } from "./article";
+
+export class News {
+  public status: string;
+  public source: string;
+  public sortBy: string;
+  public articles: Article[];
+}
