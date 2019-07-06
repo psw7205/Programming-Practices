04e750f191fdd6a1f0b4944dcbd07e55a28abd30
unknown <phsu2021@sju.ac.kr>
2019-07-06 11:13:57 +0900

    수정

--- myApp/src/app/dashboard/news/news.component.ts
+++ myApp/src/app/dashboard/news/news.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { Article } from "../../../models/article";
-import { News } from "../../../models/news";
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../../../models/article';
+import { News } from '../../../models/news';
 
 @Component({
-  selector: "app-news",
-  templateUrl: "./news.component.html",
-  styleUrls: ["./news.component.css"]
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
   tmp: News = new News();
@@ -13,21 +13,21 @@ export class NewsComponent implements OnInit {
   private createArticleData(): Article[] {
     const articles: Article[] = new Array();
     articles.push({
-      author: "박상우",
-      title: "Sample News",
-      desc: "Angular 연습 뉴스 컴포넌트",
-      url: "http://naver.com",
-      urlToImg: "",
-      date: "2019-07-06"
+      author: '박상우',
+      title: 'Sample News',
+      desc: 'Angular 연습 뉴스 컴포넌트',
+      url: 'http://naver.com',
+      urlToImg: '',
+      date: '2019-07-06'
     });
 
     articles.push({
-      author: "박상우",
-      title: "Sample News",
-      desc: "Angular 연습 뉴스 컴포넌트",
-      url: "http://naver.com",
-      urlToImg: "",
-      date: "2019-07-06"
+      author: '박상우',
+      title: 'Sample News',
+      desc: 'Angular 연습 뉴스 컴포넌트',
+      url: 'http://naver.com',
+      urlToImg: '',
+      date: '2019-07-06'
     });
 
     return articles;
@@ -35,9 +35,9 @@ export class NewsComponent implements OnInit {
 
   private createNewsData(): News {
     const news: News = new News();
-    news.status = "ok";
-    news.source = "PSW";
-    news.sortBy = "top";
+    news.status = 'ok';
+    news.source = 'PSW';
+    news.sortBy = 'top';
     news.articles = this.createArticleData();
     return news;
   }
