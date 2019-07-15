acbf2398c4d773370c54220a37b882b9249d4f2d
psw7205 <psw7205@gmail.com>
2019-07-16 08:27:50 +0900

    create new app

---
+++ KanbanBoard/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
