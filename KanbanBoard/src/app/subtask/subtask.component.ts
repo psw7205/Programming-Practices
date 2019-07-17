7a048403c7ec4f57a3a2582bcb7d8e782876f42b
psw7205 <psw7205@gmail.com>
2019-07-17 17:43:29 +0900

    수정

--- KanbanBoard/src/app/subtask/subtask.component.ts
+++ KanbanBoard/src/app/subtask/subtask.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, NgZone } from '@angular/core';
+
+import { SubTask } from '../model/subtask';
 
 @Component({
   selector: 'app-subtask',
@@ -6,7 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subtask.component.css']
 })
 export class SubtaskComponent implements OnInit {
-  constructor() {}
+  @Input()
+  subTask: SubTask;
+  editingsubTask = false;
+  currentTitle: string;
+  zone: NgZone;
+  constructor(private el: ElementRef) {}
 
   ngOnInit() {}
+  blurOnEnter(event) {
+    if (event.keyCode === 13) {
+      event.target.blur();
+    } else if (event.keyCode === 27) {
+      this.subTask.title = this.currentTitle;
+      this.editingsubTask = false;
+    }
+  }
+
+  editsubTask() {
+    this.editingsubTask = true;
+    this.currentTitle = this.subTask.title;
+
+    const textArea = this.el.nativeElement.getElementsByTagName('textarea')[0];
+
+    setTimeout(() => {
+      textArea.focus();
+    }, 0);
+  }
+
+  updatesubTask() {
+    if (!this.subTask.title || this.subTask.title.trim() === '') {
+      this.subTask.title = this.currentTitle;
+    }
+
+    // this._subTaskService.put(this.subTask).then(res => {
+    // this._ws.updatesubTask(this.subTask.boardId, this.subTask);
+    // });
+    this.editingsubTask = false;
+  }
 }
--- KanbanBoard/src/app/task/task.component.ts
+++ KanbanBoard/src/app/task/task.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  OnInit,
+  EventEmitter,
+  ElementRef
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { KanbanService } from '../service/kanban.service';
+import { Task } from '../model/task';
+import { SubTask } from '../model/subtask';
+import { Board } from '../model/board';
 
 @Component({
   selector: 'app-task',
@@ -6,7 +19,133 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-  constructor() {}
+  @Input()
+  task: Task;
 
-  ngOnInit() {}
+  @Input()
+  subTasks: SubTask[];
+
+  @Output()
+  public onAddsubTask: EventEmitter<SubTask>;
+
+  boards: Board[];
+  board: Board = new Board();
+  editingtask = false;
+  addsubTaskText: string;
+  currentTitle: string;
+  constructor(
+    private el: ElementRef,
+    private route: ActivatedRoute,
+    private kanbanService: KanbanService
+  ) {
+    this.onAddsubTask = new EventEmitter();
+  }
+
+  ngOnInit() {
+    const boardId = this.route.snapshot.params.id;
+    if (boardId !== undefined) {
+      this.boards = this.kanbanService.boards;
+      console.log(this.boards);
+      for (const v of this.boards) {
+        if (v.id === boardId) {
+          this.board = v;
+          break;
+        }
+      }
+    }
+  }
+
+  addtaskOnEnter(event: KeyboardEvent) {
+    if (event.keyCode === 13) {
+      this.updatetask();
+    } else if (event.keyCode === 27) {
+      this.cleadAddtask();
+    }
+  }
+
+  addsubTask() {
+    this.subTasks = this.subTasks || [];
+    const newsubTask = {
+      title: this.addsubTaskText
+    } as SubTask;
+    let selectedtask: Task;
+    for (const v of this.board.task) {
+      if (v.id === this.task.id) {
+        selectedtask = v;
+        break;
+      }
+    }
+
+    if (selectedtask.subtasks === undefined) {
+      selectedtask.subtasks = new Array();
+    }
+    selectedtask.subtasks.push(newsubTask);
+    this.subTasks = selectedtask.subtasks;
+    this.onAddsubTask.emit(newsubTask);
+  }
+
+  addsubTaskOnEnter(event: KeyboardEvent) {
+    if (event.keyCode === 13) {
+      if (this.addsubTaskText && this.addsubTaskText.trim() !== '') {
+        this.addsubTask();
+        this.addsubTaskText = '';
+      } else {
+        this.clearAddsubTask();
+      }
+    } else if (event.keyCode === 27) {
+      this.clearAddsubTask();
+    }
+  }
+  updatetask() {
+    if (this.task.title && this.task.title.trim() !== '') {
+      this.editingtask = false;
+    } else {
+      this.cleadAddtask();
+    }
+  }
+
+  cleadAddtask() {
+    this.task.title = this.currentTitle;
+    this.editingtask = false;
+  }
+
+  edittask() {
+    this.currentTitle = this.task.title;
+    this.editingtask = true;
+    const input = this.el.nativeElement
+      .getElementsByClassName('task-header')[0]
+      .getElementsByTagName('input')[0];
+
+    setTimeout(() => {
+      input.focus();
+    }, 0);
+  }
+
+  enableAddsubTask() {
+    const input = this.el.nativeElement
+      .getElementsByClassName('add-subTask')[0]
+      .getElementsByTagName('input')[0];
+
+    setTimeout(() => {
+      input.focus();
+    }, 0);
+  }
+
+  updatetaskOnBlur() {
+    if (this.editingtask) {
+      this.updatetask();
+      this.clearAddsubTask();
+    }
+  }
+
+  addsubTaskOnBlur() {
+    if (this.addsubTaskText && this.addsubTaskText.trim() !== '') {
+      this.addsubTask();
+    }
+    this.clearAddsubTask();
+  }
+
+  clearAddsubTask() {
+    this.addsubTaskText = '';
+  }
 }
